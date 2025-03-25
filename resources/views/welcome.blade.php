738042d540ea06997d6289f1e2a1b031eb7bd881
estevam02009 <manoel.neto53861@alunos.ufersa.edu.br>
2025-03-25 14:28:28 -0300

    criado os layouts básicos para a aplicação

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,27 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Styles / Scripts -->
-    @vite('resources/css/app.css')
-
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @endif
-</head>
-
-<body class="bg-gray-100">
+<x-layouts.guest_layout>
     <h1 class="text-3xl text-red-500 font-bold underline">
         Olá Mundo! Laravel
     </h1>
     <p class="font-semibold text-gray-500">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus obcaecati sapiente, inventore excepturi sequi ut culpa alias asperiores facilis amet sint praesentium voluptatem itaque nisi, molestiae assumenda sed, et minus.
     </p>
-</body>
-
-</html>
+</x-layouts.guest_layout>
